6c43a5ca948330191ddbf9e5d449ecf156262ce1
Antonio Maiorano <amaiorano@google.com>
2023-06-01 13:46:27 -0400

    Fix running manifest compiler to use toolchain version
    Otherwise you must have the Windows SDK installed and in PATH.

--- third_party/gn/dxc/build/run_system_binary.py
+++ third_party/gn/dxc/build/run_system_binary.py
@@ -4,4 +4,12 @@
 import subprocess
 import sys
 
-sys.exit(subprocess.call([sys.argv[1]] + sys.argv[2:]))
+env_file, bin, rest = sys.argv[1], sys.argv[2], sys.argv[3:]
+
+# Read the environment block from the file. This is stored in the format used
+# by CreateProcess. Drop last 2 NULs, one for list terminator, one for
+# trailing vs. separator.
+env_pairs = open(env_file).read()[:-2].split('\0')
+env_dict = dict([item.split('=', 1) for item in env_pairs])
+
+sys.exit(subprocess.call([bin] + rest, env=env_dict, shell=True))
